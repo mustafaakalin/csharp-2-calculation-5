78bb86ad734397614f7013c5cd1da99fb7469631
𝖒𝖚𝖘𝖙𝖆𝖋𝖆 <121257754+mustafaakalin@users.noreply.github.com>
2025-01-18 13:20:52 +0300

    Initialize C# project with .NET 8.0 and implement scholarship calculation logic

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Eğitim Durumuna Göre burs Hesaplama (Switch Case)
+            // lise ve ortaöğretim için 3000 ₺
+            // önlisans ve lisans için 4000 ₺
+            // yüksek lisans ve doktora için 5000 ₺
+            // eğer eğitim durumu girilmezse 0 ₺
+            // eğitim durumu yanlış girilirse 0 ₺
+            // eğitim durumu girilince burs miktarını ekrana yazdırınız.
+            start:
+            Console.WriteLine("############## EGITIM DUZEYI SECENEKLERI ##############\nlise\nortaöğretim\nön lisans\nlisans\nyüksek lisans\ndoktora\n##############\nEğitim Durumunuzu Giriniz: ");
+            string egitimDurumu = Console.ReadLine();
+            int bursMiktari = 0;
+
+            switch (egitimDurumu)
+            {
+                case "lise":
+                case "ortaöğretim":
+                    bursMiktari = 3000;
+                    Console.WriteLine("Burs Miktarınız: " + bursMiktari + " ₺");
+                    break;
+                case "ön lisans":
+                case "lisans":
+                    bursMiktari = 4000;
+                    Console.WriteLine("Burs Miktarınız: " + bursMiktari + " ₺");
+                    break;
+                case "yüksek lisans":
+                case "doktora":
+                    bursMiktari = 5000;
+                    Console.WriteLine("Burs Miktarınız: " + bursMiktari + " ₺");
+                    break;
+                default:
+                    Console.WriteLine("HATALI GIRIS: PROGRAM BASA DONULUYOR ");
+                    goto start;
+                    break;
+            }
+
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
